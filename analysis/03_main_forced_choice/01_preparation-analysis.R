e211e2860ee99eee77cc48bfdcbc8ff85cbeccf2
michael-franke <mchfranke@gmail.com>
2019-09-26 12:23:21 +0200

    pilot: split data analysis file, updated power calc.

--- analysis/03_main_forced_choice/01_preparation-analysis.R
+++ analysis/03_main_forced_choice/01_preparation-analysis.R
@@ -98,21 +98,4 @@ summary(m3)
 
 tapply(dat$isHigh, list(dat$cond), mean)
 
-# overall graph
-dat$cond <- factor(dat$cond, levels=c("think", "announce"))
-dat.subj <- 100*tapply(dat$isHigh, list(dat$cond, dat$output_id), mean)
-mean.subj <- apply(dat.subj, 1, mean, na.rm=T)
-print(mean.subj)
-# calculate the standard deviation
-sd.subj <- apply(dat.subj, 1, sd, na.rm=T)
-# calculate the lengths (number subjects)
-ns.subj <- apply(dat.subj, 1, length)
-# calculate the standard errors
-ses <- sd.subj/sqrt(ns.subj)
-
-par(mar = c(4, 6, 1, 6) + 2)
-xs <- barplot(mean.subj, beside=T,  ylab=c("% higher value selected"),ylim=c(0,100), cex.lab=1.5, xlab=c("cond"),  font=2, font.sub=3, las=1, legend=F, col=c("darkorange", "red"))
-par(xpd = TRUE)
-# plot the error bars
-errbar(xs, mean.subj, mean.subj+ses, mean.subj-ses, add=T, lwd=1.5, pch=26, cap=.05)
 
--- analysis/03_main_forced_choice/03_power-estimation.R
+++ analysis/03_main_forced_choice/03_power-estimation.R
@@ -1,6 +1,7 @@
 # this assumes that the data and model fits exist
 # we will target 'm3' because it does not seem to contain redundant REs
 source("analysis/03_main_forced_choice/01_preparation-analysis.R")
+# Make sure NOT  to run 02_plotting.R before as this rearranges factor levels and the following breaks!
 
 library(simr) # for simulating power
 
@@ -9,11 +10,12 @@ library(simr) # for simulating power
 # => we had a total of 90 * 6 = 540 observation per design cell (maybe expected value, if randomized?)
 
 # hypothetical difference between conditions (think: effect size 0.4)
-fixef(m3)["condannounce"] <- 0.4
+fixef(m3)["condthink"] <- -0.5
 
 # extending to 120 participants & 20 items 
-m_power_sim = extend(m3, along = "output_id", n = 120) 
+m_power_sim = extend(m3, along = "output_id", n = 220) 
 m_power_sim = extend(m_power_sim, along = "itemName", n = 20)
 
 powerSim(m_power_sim, nsim = 2)
 pc = powerCurve(m_power_sim, nsim = 20, along = "output_id")
+plot(pc)
